806ffe3c4bc521b35522d1c33f6112b9b4e0ff2e
Dilwar Singh <dilwarsingh2706@gmail.com>
2017-08-09 16:28:39 +0530

    dillu Commit

--- app/src/main/java/evacuees/com/mbank/Home_Activity.java
+++ app/src/main/java/evacuees/com/mbank/Home_Activity.java
@@ -15,48 +15,10 @@ import evacuees.com.mbank.popups.addMoneyPopup;
 
 
 public class Home_Activity extends AppCompatActivity {
-    ListView listView;
-
-    MyButton addMoney;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        listView = (ListView) findViewById(android.R.id.list);
-
-        addMoney = (MyButton) findViewById(R.id.addMoney);
-        ArrayList<String> arrayList = new ArrayList<>();
-
-        for (int i = 0; i < 50; i++) {
-            arrayList.add("Item: " + i);
-        }
-
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList);
-
-        listView.setAdapter(adapter);
-
-        addMoney.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AddMoneyClicked();
-            }
-        });
-
-    }
-
-    private void AddMoneyClicked() {
-
-        addMoneyPopup addMoneyPopup = new addMoneyPopup(this);
-        addMoneyPopup.show();
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-
-        new MenuInflater(this).inflate(R.menu.home_menu, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-}
+    }}
