daf72c5bd0e0a6758a4d23b1f5278adef287a1dd
Dilwar Singh <dilwarsingh2706@gmail.com>
2017-08-09 14:40:38 +0530

    dillu Commit

---
+++ app/src/main/java/evacuees/com/mbank/DataSet/constants.java
@@ -0,0 +1,10 @@
+package evacuees.com.mbank.DataSet;
+
+/**
+ * Created by Dilwar Singh on 8/9/2017.
+ */
+
+public class constants {
+
+    public static String Api_Location = "http://www.evastore.esy.es/mBankApis/";
+}
