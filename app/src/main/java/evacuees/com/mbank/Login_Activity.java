410639c3209f77eb7b6c206a4cc401cb370d5d19
Dilwar Singh <dilwarsingh2706@gmail.com>
2017-08-09 18:19:13 +0530

    dillu Commit

--- app/src/main/java/evacuees/com/mbank/Login_Activity.java
+++ app/src/main/java/evacuees/com/mbank/Login_Activity.java
@@ -4,11 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import com.kosalgeek.asynctask.AsyncResponse;
+import com.kosalgeek.asynctask.PostResponseAsyncTask;
+
+import java.util.HashMap;
+
+import evacuees.com.mbank.DataSet.constants;
+
 public class Login_Activity extends AppCompatActivity {
-    TextView register;
+    TextView register, login;
+    EditText Email, Password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +26,10 @@ public class Login_Activity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         register = (TextView) findViewById(R.id.register);
+        login = (TextView) findViewById(R.id.login);
+        Email = (EditText) findViewById(R.id.email);
+        Password = (EditText) findViewById(R.id.password);
+
 
         register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,6 +46,46 @@ public class Login_Activity extends AppCompatActivity {
                 }, 1);
             }
         });
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (checker()) {
+                    LoginHit();
+                }
+            }
+        });
+
+    }
+
+    private boolean checker() {
+        if (TextUtils.isEmpty(Email.getText()) || Email.getText().length() < 7) {
+            Email.setError("Invalid Email");
+            return false;
+        } else if (TextUtils.isEmpty(Password.getText()) || Password.getText().length() < 5) {
+            Password.setError("Invalid Password");
+            return false;
+        }
+        return true;
+
+    }
+
+    private void LoginHit() {
+        String email = Email.getText().toString().trim();
+        String password = Password.getText().toString().trim();
+
+        HashMap<String, String> map = new HashMap<>();
+        map.put("email", email);
+        map.put("password", password);
+
+        AsyncResponse response = new AsyncResponse() {
+            @Override
+            public void processFinish(String s) {
+
+            }
+        };
+        PostResponseAsyncTask task = new PostResponseAsyncTask();
+        task.execute(constants.Api_Location+"login.php");
 
     }
 }
