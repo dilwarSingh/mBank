0d6d5aad1e771e56e020c637118799b5a2c4611b
Deepaksaini25 <sainideepu251@gmail.com>
2017-08-10 14:13:33 +0530

    profileDeepak

--- app/src/main/java/evacuees/com/mbank/Profile_Activity.java
+++ app/src/main/java/evacuees/com/mbank/Profile_Activity.java
@@ -8,28 +8,92 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.kosalgeek.asynctask.AsyncResponse;
+import com.kosalgeek.asynctask.PostResponseAsyncTask;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.HashMap;
 
+import evacuees.com.mbank.DataSet.constants;
+
+import static android.R.id.list;
+import static java.security.AccessController.getContext;
 import static java.util.Locale.US;
 
 public class Profile_Activity extends AppCompatActivity {
 EditText name,phoneno,email,dob;
-    Button save;
-    RadioButton gender;
+   RadioGroup gender;
+    RadioButton male,female,selectedradiobutton;
     Calendar calendar = Calendar.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        name=(EditText)findViewById(R.id.);
-        phoneno=(EditText)findViewById(R.id.);
-        email=(EditText)findViewById(R.id.);
-        dob=(EditText)findViewById(R.id.);
-        save=(Button)findViewById(R.id.);
-        gender=(RadioButton)findViewById(R.id.);
+        name=(EditText)findViewById(R.id.name);
+        phoneno=(EditText)findViewById(R.id.phone);
+        email=(EditText)findViewById(R.id.email);
+        dob=(EditText)findViewById(R.id.dob);
+        gender=(RadioGroup)findViewById(R.id.gender);
+       male=(RadioButton)findViewById(R.id.male);
+       female=(RadioButton)findViewById(R.id.female);
+        selectedgender();
+        selectdob();
+        showprofile();
+        updateprofile();
 
+    }
+
+    private void updateprofile() {
+
+    }
+
+    private void showprofile() {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("email",email);
+
+        AsyncResponse response = new AsyncResponse() {
+            @Override
+            public void processFinish(String s) {
+                if (s.isEmpty() || s.equals("")) {
+                    Toast.makeText(Profile_Activity.this, "check_internet_connection", Toast.LENGTH_SHORT).show();
+                } else {
+                    try {
+                        JSONObject jsonObject = new JSONObject(s);
+                        JSONArray jsonArray = jsonObject.getJSONArray("result");
+
+                        for (int i = 0; i < jsonArray.length(); i++) {
+                            JSONObject object = jsonArray.getJSONObject(i);
+
+                            name.setText(object.getString("name"));
+                            phoneno.setText(object.getString("phone"));
+                            email.setText(object.getString("email"));
+
+
+                        }
+
+
+
+                    } catch (Exception e) {
+                        Toast.makeText(Profile_Activity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        };
+
+        PostResponseAsyncTask task = new PostResponseAsyncTask(Profile_Activity.this, map, "Loading Paper...", response);
+        task.execute(constants.Api_Location +"");
+
+
+    }
+
+    private void selectdob() {
         final DatePickerDialog.OnDateSetListener sdate = new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
@@ -51,8 +115,23 @@ EditText name,phoneno,email,dob;
 
             }
         });
-
-
     }
+
+    private void selectedgender() {
+        gender.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(gender.getCheckedRadioButtonId()==-1){
+                    Toast.makeText(Profile_Activity.this, "please select gender", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    int selectedid= gender.getCheckedRadioButtonId();
+                    selectedradiobutton=(RadioButton)findViewById(selectedid);
+                    String gendr=selectedradiobutton.getText().toString();
+                }
+            }
+        });
+    }
+
 
 }
