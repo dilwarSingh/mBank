b6cac2c05ac864c61ede1621a2a82c32865e2041
Dilwar Singh <dilwarsingh2706@gmail.com>
2017-08-10 11:34:25 +0530

    dillu Commit

--- app/src/main/java/evacuees/com/mbank/Home_Activity.java
+++ app/src/main/java/evacuees/com/mbank/Home_Activity.java
@@ -17,5 +17,6 @@ public class Home_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
     }
 }
